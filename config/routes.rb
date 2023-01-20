17c4f9bf28e65e429a0fe04f95f201649058b48f
Ricardovcn <ricardovitorcn@gmail.com>
2023-01-19 22:30:49 -0300

    adds route for creatinr enrollment

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :web_api do
+    namespace :v1 do
+      post '/enrollment', to: 'enrollment#create'
+    end
+  end
 end
